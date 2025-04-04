7b469750decb58f336b1606d2c8a4119d5a6cf3a
haheyman <107065082+haheyman@users.noreply.github.com>
2025-04-03 21:32:43 -0500

    Update Tables.sql

--- Tables.sql
+++ Tables.sql
@@ -76,6 +76,7 @@ CREATE TABLE IF NOT EXISTS OrderItems (
 CREATE TABLE IF NOT EXISTS TechInventory (
     SKU_Number INT NOT NULL,
     TechID INT NOT NULL,
+    Item_Quantity INT NOT NULL,
     Deleted ENUM('Yes', 'No') DEFAULT 'No',
     PRIMARY KEY (SKU_Number, TechID),
     FOREIGN KEY (SKU_Number) REFERENCES Inventory(SKU_Number) ON DELETE CASCADE,
